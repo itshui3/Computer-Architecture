65082d978b03897b6063c40463cb68268d166929
Hui <jeffrey.x.hui@gmail.com>
2020-05-19 16:27:08 -0700

    monday mvp seems to be hit

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,3 +1,12 @@
+# Inventory Notes:
+#   file contains a class called CPU
+#   it has an empty dunder init
+#   a load method with some logic inside
+#   an alu method that takes 3 params. 'op' 'reg_a', and 'reg_b'
+#   alu seems to want to receive 'add' calculations
+#   trace method looks like a print specificaally for seeing into current cpu state
+#   run doesn't seem indicative of what it's supposed to do
+
 """CPU functionality."""
 
 import sys
@@ -7,7 +16,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -19,26 +30,39 @@ class CPU:
         program = [
             # From print8.ls8
             0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
+            0b00000000, # LDI expects reg in pc+1
+            0b00001000, # LDI expects reg in pc+2
             0b01000111, # PRN R0
-            0b00000000,
+            0b00000000, # PRN expects a reg in next pc
             0b00000001, # HLT
         ]
+
+        self.pc = address
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+        self.trace()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == 'MULT':
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+    
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR):
+        # Do I have to check if it's 1 byte in binary? 
+        self.ram[MAR] = MDR
 
     def trace(self):
         """
@@ -46,7 +70,7 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
+        print(f"TRACE: %02X | %02X %02X %02X |" % ( # What is this string?
             self.pc,
             #self.fl,
             #self.ie,
@@ -62,4 +86,21 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while True:
+# LDI expects 2 params(regIndex, value)
+            if self.ram[self.pc] == 0b10000010: # LDI
+                ldi_param_reg = self.ram[self.pc + 1]
+                ldi_param_val = self.ram[self.pc + 2]
+
+                self.reg[ldi_param_reg] = ldi_param_val
+
+                self.pc += 3
+            
+            if self.ram[self.pc] == 0b01000111: # PRN
+                prn_param_reg = self.ram[self.pc + 1]
+                print(self.reg[prn_param_reg])
+
+                self.pc += 2
+# HLT
+            if self.ram[self.pc] == 0b00000001:
+                break
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,3 +1,9 @@
+# Inventory Notes
+# I import sys
+# from cpu I import everything
+# I create a cpu object
+# then I run load() and then run()
+
 #!/usr/bin/env python3
 
 """Main."""
@@ -5,7 +11,7 @@
 import sys
 from cpu import *
 
-cpu = CPU()
+cpu = CPU() # I create a cpu class
 
-cpu.load()
-cpu.run()
+cpu.load() # I call the .load() method
+cpu.run() # I call the .run() method, this should run some sort of protocol until a halt command is received
