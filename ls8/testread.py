3cfd6b719351104e0674d525361a2d921bb6f842
Hui <jeffrey.x.hui@gmail.com>
2020-05-20 11:13:50 -0700

    testread successfully reading bytes from file for print8

--- ls8/testread.py
+++ ls8/testread.py
@@ -1,3 +1,13 @@
-with open('print8.ls8', 'r') as print8:
+import os
+print(os.path.abspath("ls8/print8.ls8"))
+
+with open(os.path.abspath("ls8/print8.ls8"), 'r') as print8:
     for p in print8:
-        print(p)
+        printMe = ''
+        for c in p:
+            if c != str(1) and c != str(0):
+                break
+            else:
+                printMe = printMe + c
+        if len(printMe) > 0:
+            print(printMe)
